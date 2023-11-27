cde9ecc02ced94d9ba80dd2139e682a4090f1fa1
osman-butt <butt.osman@gmail.com>
2023-11-27 13:50:39 +0100

    Added Prisma ORM

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { productsRouter } from "./api/v1/products/products.js";
 
 // Globals
 const port = process.env.PORT || 3000;
@@ -15,6 +16,8 @@ app.use(cors());
 app.get(`/api/v1/`, (req, res) => {
   res.json({ message: "Nemlig.com API V1" });
 });
+
+app.use("/products", productsRouter);
 
 app.listen(port, (req, res) => {
   console.log(`The server is running on "http://localhost:${port}/api/v1"`);
