20275077e13ba5c081a15ecd52414f8f99a27965
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-28 10:57:11 +0100

    Update favoritesModel.js

--- api/v1/favorites/favoritesModel.js
+++ api/v1/favorites/favoritesModel.js
@@ -2,31 +2,33 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
 // VI KAN GETTE PÅ BEGGE MÅDER SOM NEDENFOR - ENTEN JOINER VI PRODUCTS IND PÅ FAVORITES, ELLER OGSÅ SÅ JOINER VI FAVORITES IND PÅ PRODUCTS
 async function getFavoritesFromDB() {
-  return await prisma.favorite.findMany({
+  return await prisma.product.findMany({
+    where: {
+      favorites: {
+        some: {},
+      },
+    },
     include: {
-      products: {
-        include: {
       labels: true,
       categories: true,
       inventory: true,
       // images: true,
       prices: true,
+      favorites: true,
     },
-  },
-  },
   });
 }
 
 async function getFavoriteByIdFromDB(productId) {
   return await prisma.product.findUnique({
-    where: { 
+    where: {
       product_id: productId,
       favorites: {
-        some: {} },
+        some: {},
       },
+    },
     include: {
       labels: true,
       categories: true,
@@ -38,13 +40,13 @@ async function getFavoriteByIdFromDB(productId) {
   });
 }
 
-async function postFavoriteInDB(productId, customerId){
+async function postFavoriteInDB(productId, customerId) {
   return await prisma.favorite.create({
-    data:{
+    data: {
       product_id: productId,
-      customer_id: customerId
-    }
-  })
+      customer_id: customerId,
+    },
+  });
 }
 
 export { getFavoritesFromDB, getFavoriteByIdFromDB, postFavoriteInDB };
