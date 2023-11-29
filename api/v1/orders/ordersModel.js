0e8a4b0024179c4928e4da0e0bedaf5923ccec25
gustavwiese <gusse12345@gmail.com>
2023-11-29 16:28:01 +0100

    create still not working..

--- api/v1/orders/ordersModel.js
+++ api/v1/orders/ordersModel.js
@@ -4,24 +4,23 @@ const prisma = new PrismaClient();
 
 async function getOrdersFromDB() {
   return await prisma.order.findMany({
-    // WHICH RELATIONS TO INCLUDE IN THE RESPONSE?
     include: {
-      order_items: true,
-      // include: {
-      // products: true,
-      // }
+      order_items: {
+        include: {
+          products: true,
+        }
+      },
     },
   });
 }
 
 // CREATE A NEW ORDER IN THE DATABASE
-async function createOrderInDB(customer_id, adress_id) {
+async function createOrderInDB() {
   return await prisma.order.create({
     data: {
       // WHICH DATA TO INSERT?
-      customer_id: customer_id,
-      adress_id: adress_id,
-    },
+      
+    }
   });
 }
 
