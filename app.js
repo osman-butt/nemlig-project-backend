3841af7d7441d983768d16d9eeb213eeb2abd0b3
osman-butt <butt.osman@gmail.com>
2023-11-30 14:44:36 +0100

    authRouter added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { productsRouter } from "./api/v1/products/products.js";
 import { favoritesRouter } from "./api/v1/favorites/favorites.js";
+import { authRouter } from "./api/v1/authentication/auth.js";
 
 // Globals
 const port = process.env.PORT || 3000;
@@ -20,6 +21,7 @@ app.get(`/api/v1/`, (req, res) => {
 
 app.use("/products", productsRouter);
 app.use("/favorites", favoritesRouter);
+app.use("/api/v1", authRouter);
 
 app.listen(port, (req, res) => {
   console.log(`The server is running on "http://localhost:${port}/api/v1"`);
