4d69de67d6247647dbd4d2f64bee82ed928f9617
osman-butt <butt.osman@gmail.com>
2023-11-30 23:07:08 +0100

    Removed comments

--- api/v1/authentication/authModel.js
+++ api/v1/authentication/authModel.js
@@ -6,11 +6,6 @@ async function getUsers() {
   return await prisma.user.findMany({
     include: {
       roles: true,
-      //   customer: {
-      //     include: {
-      //       addresses: true,
-      //     },
-      //   },
     },
   });
 }
