cf213016ab42e5ef7d33976973c864543e9eb89b
osman-butt <butt.osman@gmail.com>
2023-11-30 15:37:03 +0100

    Added rate limiting to login route

--- api/v1/authentication/auth.js
+++ api/v1/authentication/auth.js
@@ -1,9 +1,19 @@
 import express from "express";
 import authController from "./authController.js";
+import rateLimit from "express-rate-limit";
 
 const authRouter = express.Router();
 
-authRouter.post("/login", authController.loginUser);
+// Rate limiting to avoid bruce force attacks
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // 5 login attempts per windowMs
+  message: {
+    message: "Too many failed login attempts, please try again later.",
+  },
+});
+
+authRouter.post("/login", loginLimiter, authController.loginUser);
 authRouter.post("/register", authController.registerUser);
 authRouter.get("/logout", authController.logoutUser);
 
