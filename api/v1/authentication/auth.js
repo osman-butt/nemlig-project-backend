7835aa1e88d8969c5d3f47cb0ef989aed4b8b90c
osman-butt <butt.osman@gmail.com>
2023-11-30 15:04:47 +0100

    Removed comments

--- api/v1/authentication/auth.js
+++ api/v1/authentication/auth.js
@@ -1,14 +1,7 @@
 import express from "express";
 import authController from "./authController.js";
-// import  from "./authModel.js";
 
 const authRouter = express.Router();
-
-// GET
-// authRouter.get("/register", async (req, res) => {
-//   const customers = await getUsers();
-//   res.send(customers);
-// });
 
 authRouter.post("/login", authController.loginUser);
 authRouter.post("/register", authController.registerUser);
