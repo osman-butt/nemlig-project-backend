587c2d027be6af0d5fb752c963087f8e4a4238b9
osman-butt <butt.osman@gmail.com>
2023-12-11 11:11:05 +0100

    Create order as transaction

--- api/v1/orders/ordersModel.js
+++ api/v1/orders/ordersModel.js
@@ -58,6 +58,12 @@ async function updateCartInDB(cart_item_id, quantity) {
   });
 }
 
+async function deleteCartItemInDB(cart_item_id) {
+  return await prisma.cart_item.delete({
+    where: { cart_item_id: cart_item_id },
+  });
+}
+
 async function getCartFromDb(customer_id) {
   return await prisma.cart.findFirst({
     where: {
@@ -82,6 +88,55 @@ async function getCartFromDb(customer_id) {
   });
 }
 
+async function createOrderTransaction(inventory, cart, order) {
+  // Transaction:
+  // Update inventory
+  // create order and order_items from cart
+  // Delete cart
+  return prisma.$transaction(async tx => {
+    // 1. Update inventory for each product
+    const newInventory = await inventory.forEach(async product => {
+      return await prisma.inventory.update({
+        where: {
+          product_id: product.product_id,
+        },
+        data: {
+          inventory_stock: product.inventory_stock,
+        },
+      });
+    });
+    // 2. Create order and order_items
+    const newOrder = await prisma.order.create({
+      data: {
+        customer_id: order.customer_id,
+        address_id: order.address_id,
+        order_date: new Date().toISOString(),
+        order_items: {
+          createMany: {
+            data: order.order_items,
+          },
+        },
+      },
+    });
+
+    // 3. Delete cart
+    const newCart = await prisma.cart.update({
+      where: {
+        cart_id: cart.cart_id,
+      },
+      data: {
+        cart_items: {
+          deleteMany: {},
+        },
+      },
+      include: {
+        cart_items: true,
+      },
+    });
+    return newOrder;
+  });
+}
+
 export {
   getOrdersFromDB,
   createOrderInDB,
@@ -89,4 +144,6 @@ export {
   updateInventoryInDB,
   updateCartInDB,
   getCartFromDb,
+  createOrderTransaction,
+  deleteCartItemInDB,
 };
