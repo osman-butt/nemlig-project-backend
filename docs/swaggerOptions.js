03ad8c857ab4eb36c920e866e290a3571d583ed8
osman-butt <butt.osman@gmail.com>
2023-12-14 21:18:54 +0100

    Update swaggerOptions.js

--- docs/swaggerOptions.js
+++ docs/swaggerOptions.js
@@ -1,21 +1,23 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000/api/v1";
+
 const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Exam Ecommerce API",
-            version: "1.0.0",
-            description: "An Express Ecommerce API",
-        },
-        servers: [
-            {
-                url: "http://localhost:3000/api/v1",
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Exam Ecommerce API",
+      version: "1.0.0",
+      description: "An Express Ecommerce API",
     },
-    apis: ["./docs/*.js"],
-}
+    servers: [
+      {
+        url: FRONTEND_URL,
+      },
+    ],
+  },
+  apis: ["./docs/*.js"],
+};
 
 const specs = swaggerJSDoc(options);
 
